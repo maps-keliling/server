410f9cffcc24a277616836f540049726309f240c
ranggakusuma <ranggavskusuma@gmail.com>
2019-01-25 16:07:46 +0700

    add mlab database

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 const indexRouter = require('./routes/index');
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/keliling', { useNewUrlParser: true });
+mongoose.connect('mongodb://admin:admin123@ds211865.mlab.com:11865/final-project', { useNewUrlParser: true });
 const app = express();
 
 app.use(cors())
